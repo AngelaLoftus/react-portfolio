2c8a932b59e600f0aef48e54e0190be731101cde
Angela Loftus <angelaloftus@yahoo.com>
2021-12-18 14:43:35 -0600

    initial commit with header and footer

---
+++ loftus-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+  <div class = "container">
+  <Header/>
+  <Footer /></div>
+  )
+}
+
+export default App;

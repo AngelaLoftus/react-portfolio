ab19059f053d7998e96f947a19807a069552afb2
Angela Loftus <angelaloftus@yahoo.com>
2021-12-20 22:53:01 -0600

    set up pages and nav component

--- loftus-portfolio/src/App.js
+++ loftus-portfolio/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
 import Footer from './components/Footer';
+import PortfolioContainer from './components/PortfolioContainer'
 
 function App() {
   return (
-  <div class = "container">
-  <Header/>
+  <div>
+  <div>  <PortfolioContainer/></div>
+
   <Footer /></div>
   )
 }
---
+++ loftus-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import MyPortfolio  from './pages/MyPortfolio';
+import Contact from './pages/Contact';
+import Header from './Header';
+
+function PortfolioContainer() {
+  // Using useState, set the default value for currentPage to 'About'
+  const [currentPage, handlePageChange] = useState('About');
+
+  // The renderPage method uses a switch statement to render the appropriate current page
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'About':
+        return <About />;
+      case 'My Portfolio':
+        return <MyPortfolio />;
+      case 'Contact':
+        return <Contact />;
+        case 'Resume':
+            return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+
+  return (
+    <div>
+      {/* Pass the state value and the setter as props to NavTabs */}
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Header/>
+      {/* Call the renderPage function passing in the currentPage */}
+      <div>{renderPage(currentPage)}</div>
+    </div>
+  );
+}
+
+export default PortfolioContainer;

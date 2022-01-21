2cd29d553a7368e0f697481ce422f94e4cbc7dc7
Angela Loftus <angelaloftus@yahoo.com>
2022-01-20 23:12:17 -0600

    styling ongoing

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -3,10 +3,14 @@ import React from "react";
 const About = () => (
   <div>
     <h1>About Page</h1>
-    {/* <img src = "" alt="">PICTURE OF ME</img> */}
-    <p>
-    I am a budding web developer residing in Austin, TX. With a background in mathematics education, I spent three years teaching secondary algebra. I have a B.B.A. in Marketing from the U.T. Austin McCombs School of Business. I recently earned a certificate from the UT Coding Boot Camp in Full Stack Web Development. I am passionate about bringing my skills in mathematics together with my knowledge of business best practices and branding in order to provide high-quality, user-friendly applications that can generate market interest. I can tackle projects utilizing the full-stack of web technologies such as React, Node.js, MongoDB, and Express. I look forward to contributing to a professional team in this fast-paced and exciting industry.
-    </p>
+    <div>
+      <img src = "https://lh3.googleusercontent.com/cIs_dLER9r4kzkAOYd1P-BgXT-PNUeMWpsubV-aFrT74Vw2KfGB_ZftDmH2LyRiZ6SYvwVTG6YZ6vhkOIhlgCTc5JHE-PkIgyLTZ7t8sKjV4BWBOahGdAF0VgZwSg1XgBYOXEEo8fqegHaKe6EiSLrw91TtmOIPhQyCDShhG9xlPnKOro6VJ20GPycEu0S-XLNYrvvIgL4-JCXERm7FaQimzsn9c2dQjhIccdphPHqsbuUSMKDgPfGxDkBln4-u_VXe7hpZ5B-6rDBoLWFx-9tRfj8RoPUu0NgDq8X6rwwRg8zZR6kKVJse-eGagaqOHK8woiBLdYavt5PwNv3Tf6cL-hsMSLOXIYGzMg-CsQadW44sEbCUk7zweE-B3N9SLj8xY3rt1ZzHV_mysawzWSZZ5Yp97FHGhXFOyrWrF-IpsMRdVTa-AuBA9PWkUopu2boE9rN7B5vlm0nKem7z6_UWSb0P4QXxpVkooIyTyu-f2H9xJmtho-hL2IHe9PafB9nej3WsUQBxkWERH-zCm8PG4obzpQwfAueuCwpsZigKmZUxC6Wzs2wrwoT_66j8flJ6U__IUd6Jg6R-Z0XiYU97_VvFilS0Hyyypoqan-ehQR4gs6WkIIEJ0nUdmBeF3NMmAzLxuOsq5I3Usz-vs05cwRdR8qd09BPl7__PnLSg209KIZvh06xtliIVyZHP6-6aqVWau9euhMtYx5ih-AaG7Sg=w417-h880-no?authuser=0" alt=""></img>
+    </div>
+    <div>
+      <p>
+      I am a budding web developer residing in Austin, TX. With a background in mathematics education, I spent three years teaching secondary algebra. I have a B.B.A. in Marketing from the U.T. Austin McCombs School of Business. I recently earned a certificate from the UT Coding Boot Camp in Full Stack Web Development. I am passionate about bringing my skills in mathematics together with my knowledge of business best practices and branding in order to provide high-quality, user-friendly applications that can generate market interest. I can tackle projects utilizing the full-stack of web technologies such as React, Node.js, MongoDB, and Express. I look forward to contributing to a professional team in this fast-paced and exciting industry.
+      </p>
+    </div>
   </div>
 );
 
